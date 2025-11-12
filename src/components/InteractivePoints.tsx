2ac642395c1ef9f0267241a0069c58cfa92bc638
Sanekx Arcs <56607007+SanekxArcs@users.noreply.github.com>
2025-11-12 15:21:18 +0100

    Updated InteractivePoints.tsx

--- src/components/InteractivePoints.tsx
+++ src/components/InteractivePoints.tsx
@@ -18,7 +18,7 @@ interface Point {
 
 const points: Point[] = [
   { id: 1, x: 21, y: 46, initialX: 50, initialY: 50, isBlack: false, label: 'Strategieberatung', initialScale: 0, hoverScale: 1.8, magneticRadius: 20, size: 40 },
-  { id: 2, x: 63, y: 28, initialX: 50, initialY: 50, isBlack: false, label: 'Netzwerkzugang', initialScale: 0, hoverScale: 1.8, magneticRadius: 20, size: 40 },
+  { id: 2, x: 39, y: 13, initialX: 50, initialY: 50, isBlack: false, label: 'Netzwerkzugang', initialScale: 0, hoverScale: 1.8, magneticRadius: 20, size: 40 },
   { id: 3, x: 71, y: 18, initialX: 50, initialY: 50, isBlack: false, label: 'Technologietransfer', initialScale: 0, hoverScale: 1.8, magneticRadius: 20, size: 40 },
   { id: 4, x: 90, y: 52, initialX: 50, initialY: 50, isBlack: false, label: 'Lizenzpartner', initialScale: 0, hoverScale: 1.8, magneticRadius: 20, size: 40 },
   { id: 5, x: 26, y: 46, initialX: 50, initialY: 50, isBlack: true, label: 'Montage-partner', initialScale: 0, hoverScale: 4, magneticRadius: 20, size: 146 },
