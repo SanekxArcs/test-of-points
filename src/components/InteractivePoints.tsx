3a4294f336c426e1d1c669432d809bf704f00e91
Sanekx Arcs <56607007+SanekxArcs@users.noreply.github.com>
2025-11-12 15:41:02 +0100

    Updated InteractivePoints.tsx

--- src/components/InteractivePoints.tsx
+++ src/components/InteractivePoints.tsx
@@ -25,7 +25,7 @@ const points: Point[] = [
   { id: 6, x: 65, y: 70, initialX: 50, initialY: 50, isBlack: false, label: 'Lager & Logistik', initialScale: 0, hoverScale: 1, magneticRadius: 20, size: 40 },
   { id: 7, x: 82, y: 78, initialX: 50, initialY: 50, isBlack: false, label: 'After sales', initialScale: 0, hoverScale: 1, magneticRadius: 20, size: 40 },
   { id: 8, x: 95, y: 65, initialX: 50, initialY: 50, isBlack: false, label: 'Exklusivvertrieb', initialScale: 0, hoverScale: 1, magneticRadius: 20, size: 40 },
-  { id: 9, x: 55, y: 117, initialX: 50, initialY: 50, isBlack: true, label: 'Distributionspartner', initialScale: 0, hoverScale: 4, magneticRadius: 50, size: 64 },
+  { id: 9, x: 55, y: 90, initialX: 50, initialY: 50, isBlack: true, label: 'Distributionspartner', initialScale: 0, hoverScale: 4, magneticRadius: 50, size: 64 },
   { id: 10, x: 42, y: 70, initialX: 50, initialY: 50, isBlack: false, label: 'Fortbildungsprogramme', initialScale: 0, hoverScale: 1, magneticRadius: 20, size: 40 },
 ];
 
