730bf26470c7b9aa1fa971d8f263e07f960ad74e
Sanekx Arcs <56607007+SanekxArcs@users.noreply.github.com>
2025-11-12 15:44:08 +0100

    Updated InteractivePoints.tsx

--- src/components/InteractivePoints.tsx
+++ src/components/InteractivePoints.tsx
@@ -19,7 +19,7 @@ interface Point {
 const points: Point[] = [
   { id: 1, x: 66, y: 10, initialX: 50, initialY: 50, isBlack: false, label: 'Strategieberatung', initialScale: 0, hoverScale: 1, magneticRadius: 20, size: 40 },
   { id: 2, x: 39, y: 13, initialX: 50, initialY: 50, isBlack: false, label: 'Netzwerkzugang', initialScale: 0, hoverScale: 1, magneticRadius: 20, size: 40 },
-  { id: 3, x: 71, y: 18, initialX: 50, initialY: 50, isBlack: false, label: 'Technologietransfer', initialScale: 0, hoverScale: 1, magneticRadius: 20, size: 40 },
+  { id: 3, x: 57, y: 29, initialX: 50, initialY: 50, isBlack: false, label: 'Technologietransfer', initialScale: 0, hoverScale: 1, magneticRadius: 20, size: 40 },
   { id: 4, x: 90.5, y: 38.5, initialX: 50, initialY: 50, isBlack: true, label: 'Lizenzpartner', initialScale: 0, hoverScale: 4, magneticRadius: 50, size: 64 },
   { id: 5, x: 20.8, y: 47.5, initialX: 50, initialY: 50, isBlack: true, label: 'Montage-partner', initialScale: 0, hoverScale: 4, magneticRadius: 50, size: 146 },
   { id: 6, x: 65, y: 70, initialX: 50, initialY: 50, isBlack: false, label: 'Lager & Logistik', initialScale: 0, hoverScale: 1, magneticRadius: 20, size: 40 },
