778a33eb2f5119bf603ad12f3e73c023246a76b6
Sanekx Arcs <56607007+SanekxArcs@users.noreply.github.com>
2025-11-12 16:01:13 +0100

    Updated InteractivePoints.tsx

--- src/components/InteractivePoints.tsx
+++ src/components/InteractivePoints.tsx
@@ -42,7 +42,6 @@ const connections = [
   { from: 3, to: 4, isBlack: false },
   { from: 3, to: 6, isBlack: false },
   { from: 4, to: 5, isBlack: true },
-  { from: 4, to: 8, isBlack: false },
   { from: 4, to: 9, isBlack: true },
   { from: 5, to: 6, isBlack: false },
   { from: 5, to: 9, isBlack: true },
