ed90afe848ad105f72b6b1b7f6679b70fc4c98be
Oleksandr Dzisiak <dzisiak@twnty.de>
2025-11-18 12:26:42 +0100

    lg done

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,8 +6,8 @@ function HeroSection() {
     <section className="lg:h-screen h-fit max-h-270 font-neue-haas font-normal relative overflow-hidden bg-linear-to-b from-[#D3D3D3] to-[#FFDD66]">
       <div className="flex h-full items-center max-w-480 mx-auto">
         <div className="grid grid-cols-1 gap-6 md:gap-0 lg:grid-cols-2 xl:gap-12 w-full items-center">
-          <div className="pl-5.5 md:pl-12.5 xl:pl-35 pt-12.5 w-full md:max-w-2/3 lg:max-w-full">
-            <h1 className="text-[1.6875rem] md:text-[2.5625rem] text-black text-balance leading-[121%] tracking-[-0.05em] mb-2">
+          <div className="px-5.5 md:pl-12.5 lg:pl-14.5 xl:pl-35 pt-12.5 w-full md:max-w-2/3 lg:max-w-full">
+            <h1 className="text-[1.6875rem] md:text-[2.5625rem] text-black text-balance leading-[121%] tracking-[-0.05em] mb-2 lg:mb-10">
               Deine Plattform für eine nachhaltige und digital vernetzte Energiezukunft
             </h1>
             <p className="text-[1.125rem] md:text-[1.375rem] text-white">The transition starts now.</p>
